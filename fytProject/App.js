cc8b3299eacf93bfa4a907030f1b56f588b091f8
usersblock <thanhphuocly@hotmail.com>
2018-12-24 02:45:35 -0500

    Add files via upload

--- fytProject/App.js
+++ fytProject/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import FormTrain from './components/FormTrain';
 
 export default class App extends React.Component {
   state = {
@@ -21,7 +21,8 @@ export default class App extends React.Component {
       return (
         <View style={styles.container}>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
+          <FormTrain type={["double","double","double","double","string"]} name={["Age","Height","Weight","BMI","Lastinfo"]}/>
+
         </View>
       );
     }
@@ -38,7 +39,9 @@ export default class App extends React.Component {
         ...Icon.Ionicons.font,
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+        
+'Roboto_medium': require('./assets/fonts/Roboto-Medium.ttf'),
+
       }),
     ]);
   };
@@ -57,7 +60,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f00',
+    backgroundColor: '#fff',
   },
 span: {
 margin: 1,}
