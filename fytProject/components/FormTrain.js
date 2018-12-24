18d899e6b64f10720274cb0a77c499f71c83d2ff
usersblock <thanhphuocly@hotmail.com>
2018-12-24 02:46:06 -0500

    Add files via upload

--- fytProject/components/FormTrain.js
+++ fytProject/components/FormTrain.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Image,Input,Form,TextInput,TouchableOpacity} from 'react-native';
-import { Button,Card } from 'react-native-material-ui';
+import {  StyleSheet, Image,TextInput} from 'react-native';
+import {Button,Card,Text,View,Form,Item,Input} from 'native-base';
+
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 
 export default class FormTrain extends React.Component {
   constructor(props) {
@@ -15,8 +17,40 @@ typeholder:this.props.type,
 
 
   this.handleClick = this.handleClick.bind(this);
+this.handleBackClick = this.handleBackClick.bind(this);
 }
-handleClick() {
+
+
+onSwipeRight(gestureState){
+if(this.state.counter>0){
+this.setState({
+counter:this.state.counter-1,
+});
+}}
+onSwipeLeft(gestureState){
+console.log(this.props.name);
+let tempholder=this.state.inputholder.slice()
+tempholder[this.state.counter]=this.state.statistic;
+if(this.state.counter<this.state.nameholder.length-1){
+  this.setState({
+      inputholder:tempholder,counter:this.state.counter+1,
+	statistic:"",
+    });
+console.log("condition working");}
+else{
+this.setState({
+      inputholder:tempholder,counter:this.state.counter+1,
+	statistic:"",
+    });
+console.log("Go to next screen");
+}
+
+}
+
+
+
+handleClick(event) {
+console.log(this.props.name);
 let tempholder=this.state.inputholder.slice()
 tempholder[this.state.counter]=this.state.statistic;
 if(this.state.counter<this.state.nameholder.length-1){
@@ -34,42 +68,73 @@ console.log("Go to next screen");
 }
   }
 
+
+handleBackClick(event){
+if(this.state.counter>0){
+this.setState({
+counter:this.state.counter-1,
+});
+}}
+
+
+
   render() {
 	let percent=(this.state.counter/this.state.nameholder.length)*100;
 percent=percent.toString()+"%";
     console.log(this.state.counter);
 console.log(percent);
 console.log(this.state.inputholder);
-
+   const config = {
+      velocityThreshold: 0.3,
+      directionalOffsetThreshold: 80
+    };
     return (
-      <Card style={{backgroundColor:'pink'}}>
+ <GestureRecognizer
+        onSwipeLeft={(state) => this.onSwipeLeft(state)}
+        onSwipeRight={(state) => this.onSwipeRight(state)}
+        config={config}
+        
+        >
+
+
+
+
+
+      <Card>
 <View style={{flexDirection:"row"}}>
 
+<Button text="Back" style={styles.buttonView} onPress={this.handleBackClick} title="Back" name="Back">
+<Text style={styles.textViewtwo}>Back</Text>
+</Button>
       <Text style={styles.textView}>Please Input {this.state.nameholder[this.state.counter]}</Text>
 
-<TouchableOpacity text="Submit" style={styles.buttonView} onPress={this.handleClick} title="Submit">
+<Button text="Submit" style={styles.buttonView} onPress={this.handleClick} title="Submit" name="Submit">
 
 <Text style={styles.textViewtwo}>Submit</Text>
 
-</TouchableOpacity>
+</Button>
 
 </View>
 
 <View style={{width:percent,backgroundColor:'#3cc11f',height:"2%",marginTop:"1%",marginBottom:"1%",
 }}></View>
 
-<Text>{this.state.nameholder[this.state.counter]}</Text>
+<Text style={{fontSize:30}}> {this.state.nameholder[this.state.counter]}</Text>
 
 	<View style={{backgroundColor:'#ffffff'}}>
-
-      <TextInput style={styles.textInputView}
+<Form>
+<Item>
+      <Input style={styles.textInputView}
     onChangeText={(statistic) => this.setState({statistic})}
     value={this.state.statistic}
       placeholder="Type here"/>
-
+</Item>
+</Form>
 	</View>
 
       </Card>
+</GestureRecognizer>
+
     );
   }
 }
@@ -102,7 +167,7 @@ minWidth:1000,
 backgroundColor:'#f98b7a',},
 
 textView:{
-width:'80%',
+width:'50%',
 height:50,
 backgroundColor:'#FA5845',
 textAlign:'left',
@@ -120,13 +185,13 @@ alignItems: 'center',
 textInputView:{
 margin:10,
 height:40,
-fontSize:20,
+fontSize:30,
 backgroundColor:'#e5e5e5',
 color:'#FFFFFF',},
 
 
 buttonView:{
-width:'20%',
+width:'25%',
 height:50,
 textAlign:'center',
 backgroundColor:'#FA5845',
@@ -138,6 +203,6 @@ height:'20%',
 },
 
 textViewtwo:{
-fontSize:20,
+fontSize:15,
 color:'#FFFFFF',}
 });
