ba99ba4bee7af5ebca29d8e6267b6cd31e894a22
Daniel <dmconnol@buffalo.edu>
2018-12-25 01:22:49 -0500

    Smoothing out any merge problems

--- fytProject/App.js
+++ fytProject/App.js
@@ -14,14 +14,7 @@ const styles = StyleSheet.create({
   },
 });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
 
 export default class App extends React.Component {
 
