0aa686af761bbbb9e0df17e04ff33825ad96cd3e
Luis Lopez Cardona <lalopezc@protonmail.com>
2018-12-27 19:58:24 -0500

    Added frontend playlist controls #16
    * Created playlist folder
    * Created playlist view which will contain all related components from the playlist mockup
    * Created controls component which contains three buttons (next, check and previous)

---
+++ fytProject/components/Playlist/Playlist.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Container, Header, Left, Button, Body, Right, Icon, Title, Content } from 'native-base';
+import Controls from './Controls'
+
+const styles = StyleSheet.create({
+    bg_dark: {
+      backgroundColor: '#303030'
+    },
+    header: {
+      backgroundColor: '#B71C1C'
+    }
+  });
+
+export default class Playlist extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Container style={styles.bg_dark}>
+        <Header style={styles.header}>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Fyt</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Controls></Controls>
+      </Container>
+    );
+  }
+}
