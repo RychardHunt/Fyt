59c7695440ef486fe0af21587d47cd039a05bd1a
Luis Lopez Cardona <lalopezc@protonmail.com>
2018-12-27 22:21:41 -0500

    Made things pretty #22
    Used prettier

--- fytProject/components/Playlist/Controls.js
+++ fytProject/components/Playlist/Controls.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react';
-import { Button, Body, Icon } from 'native-base';
-import { View, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import { Button, Body, Icon } from "native-base";
+import { View, StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   play_button: {
     borderRadius: 50,
-    backgroundColor: 'transparent',
-    borderColor: '#fff',
-    borderStyle: 'solid',
+    backgroundColor: "transparent",
+    borderColor: "#fff",
+    borderStyle: "solid",
     borderWidth: 2,
     elevation: 0,
     width: 100,
     height: 100,
-    justifyContent: 'center',
-    alignSelf: 'center'
+    justifyContent: "center",
+    alignSelf: "center"
   },
   container: {
     padding: 10,
     paddingBottom: 25,
     flex: 0.25,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-    position: 'absolute',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    position: "absolute",
     bottom: 0
   },
   check: {
     fontSize: 50
   },
   arrow_button: {
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
     elevation: 0,
-    alignSelf: 'center',
+    alignSelf: "center",
     width: 100,
     height: 100
   },
@@ -49,13 +49,21 @@ export default class Controls extends Component {
     return (
       <View row style={styles.container}>
         <Button style={styles.arrow_button}>
-          <Icon style={styles.arrow} type="MaterialIcons" name="keyboard-arrow-left"></Icon>
+          <Icon
+            style={styles.arrow}
+            type="MaterialIcons"
+            name="keyboard-arrow-left"
+          />
         </Button>
         <Button style={styles.play_button}>
-          <Icon style={styles.check} type="Feather" name="check"/>
+          <Icon style={styles.check} type="Feather" name="check" />
         </Button>
         <Button style={styles.arrow_button}>
-          <Icon style={styles.arrow} type="MaterialIcons" name="keyboard-arrow-right"></Icon>
+          <Icon
+            style={styles.arrow}
+            type="MaterialIcons"
+            name="keyboard-arrow-right"
+          />
         </Button>
       </View>
     );
--- fytProject/components/Playlist/Playlist.js
+++ fytProject/components/Playlist/Playlist.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Container, Header, Left, Button, Body, Right, Icon, Title, Content } from 'native-base';
-import Controls from './Controls'
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import {
+  Container,
+  Header,
+  Left,
+  Button,
+  Body,
+  Right,
+  Icon,
+  Title,
+  Content
+} from "native-base";
+import Controls from "./Controls";
 
 const styles = StyleSheet.create({
-    bg_dark: {
-      backgroundColor: '#303030'
-    },
-    header: {
-      backgroundColor: '#B71C1C'
-    }
-  });
+  bg_dark: {
+    backgroundColor: "#303030"
+  },
+  header: {
+    backgroundColor: "#B71C1C"
+  }
+});
 
 export default class Playlist extends Component {
   constructor(props) {
@@ -23,7 +33,7 @@ export default class Playlist extends Component {
         <Header style={styles.header}>
           <Left>
             <Button transparent>
-              <Icon name='menu' />
+              <Icon name="menu" />
             </Button>
           </Left>
           <Body>
@@ -31,7 +41,7 @@ export default class Playlist extends Component {
           </Body>
           <Right />
         </Header>
-        <Controls></Controls>
+        <Controls />
       </Container>
     );
   }
