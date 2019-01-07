fe864674a0e922ae8f6ae87e1b6923ed76433bed
likhithr <likhithr@buffalo.edu>
2019-01-06 23:27:55 -0500

    Added Fireabase Functionality so users can login/register

--- fytProject/components/Login.js
+++ fytProject/components/Login.js
@@ -1,86 +1,146 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,TextInput,TouchableOpacity,KeyboardAvoidingView,StatusBar} from 'react-native';
+import React, { Component } from 'react';
+import {
+  Alert,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  StatusBar,
+} from 'react-native';
 
-    export default class Login extends Component {
-        render() {
-          return (
-        
-            <KeyboardAvoidingView behavior="padding" style={styles.container}>
-            <Text style={styles.title}>
-            Login
-            </Text>
-            <StatusBar
-            barStyle="light-content"
-            />
-            <TextInput 
-            placeholder="email"
-            placeholderTextColor='rgba(255,255,255,0.7)'
-            returnKeyLabel="next"
-            onSubmitEditing={() => this.passwordInput.focus()}
-            keyboardType="email-address"
-            autoCapitalize="none"
-            autoCorrect={false}
-            style={styles.input}
-            
-            />
-            <TextInput
-            placeholder="password"
-            placeholderTextColor='rgba(255,255,255,0.7)'
-            secureTextEntry
-            returnKeyLabel="go"
-            
-            style={styles.input}
-            ref={(input) => this.passwordInput=input}
-            />
+import * as firebase from 'firebase';
 
-            <TouchableOpacity style={styles.buttonContainer}>
-            <Text style={styles.buttonText}> LOGIN </Text>
-            </TouchableOpacity>
+const firebaseConfig = {
+  apiKey: 'AIzaSyBGxUhCLw2tqdxJyrU4HgvjoCCQriAcFKY',
+  authDomain: 'fytdatabase.firebaseapp.com',
+  databaseURL: 'https://fytdatabase.firebaseio.com',
+  projectId: 'fytdatabase',
+  storageBucket: 'fytdatabase.appspot.com',
+};
 
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
-            </KeyboardAvoidingView>
-            
-          );
-        }
-      }
-      
-      const styles = StyleSheet.create({
-       container: {
-           flex: 1,
-         backgroundColor: '#e74c3c',
-         padding: 20
-       },
-       input: { 
-           height: 40,
-           backgroundColor: 'rgba(255,255,255,0.4)',
-            marginBottom: 15,
-            color: '#FFF',
-            paddingHorizontal: 10
-       },
-       buttonContainer:{ 
-            backgroundColor: '#c0392b',
-            paddingVertical: 10,
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
 
+    Alert.alert(
+      'Welcome User:',
+      email,
+      [{ text: 'OK', onPress: () => console.log('OK Pressed') }],
+      { cancelable: false }
+    );
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+  }
+});
 
-       },
-       buttonText:{ 
-        textAlign: 'center',
-        color: '#FFFFFF',
-        fontWeight: '700'
-       },
-       title:
-       { 
-        paddingBottom: 90,
-        textAlign: 'center',
-        marginTop: 80,
-        color: '#FFFFFF',
-        fontWeight: '600',
-        fontSize: 30,
-        paddingHorizontal: 10
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
 
-
-       }
-
-
+  loginUser = (email, password) => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        Alert.alert(
+          'Error',
+          errorMessage,
+          [{ text: 'OK', onPress: () => console.log('OK Pressed') }],
+          { cancelable: false }
+        );
+        return;
       });
-    
+  };
+
+  render() {
+    return (
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <Text style={styles.title}>Login</Text>
+        <StatusBar barStyle="light-content" />
+        <TextInput
+          placeholder="email"
+          placeholderTextColor="rgba(255,255,255,0.7)"
+          returnKeyLabel="next"
+          onSubmitEditing={() => this.passwordInput.focus()}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+          style={styles.input}
+          onChangeText={email => this.setState({ email })}
+        />
+        <TextInput
+          placeholder="password"
+          placeholderTextColor="rgba(255,255,255,0.7)"
+          secureTextEntry
+          returnKeyLabel="go"
+          style={styles.input}
+          onChangeText={password => this.setState({ password })}
+          ref={input => (this.passwordInput = input)}
+        />
+
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={() => this.loginUser(this.state.email, this.state.password)}>
+          <Text style={styles.buttonText}> LOGIN </Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e74c3c',
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    backgroundColor: 'rgba(255,255,255,0.4)',
+    marginBottom: 15,
+    color: '#FFF',
+    paddingHorizontal: 10,
+  },
+  buttonContainer: {
+    backgroundColor: '#c0392b',
+    paddingVertical: 10,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontWeight: '700',
+  },
+  title: {
+    paddingBottom: 90,
+    textAlign: 'center',
+    marginTop: 80,
+    color: '#FFFFFF',
+    fontWeight: '600',
+    fontSize: 30,
+    paddingHorizontal: 10,
+  },
+});
