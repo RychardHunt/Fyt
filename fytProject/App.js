5bc8737b15e5990c0a2a0c8575d3652cbad5bd42
usersblock <thomasly8991@gmail.com>
2019-01-10 20:14:33 -0500

    Update App.js

--- fytProject/App.js
+++ fytProject/App.js
@@ -4,11 +4,7 @@ import WorkoutContainer from './components/containers/WorkoutContainer';
 import { Provider } from 'react-redux';
 import store from './store';
 import NavBar from './components/NavBar/NavBar';
-<<<<<<< HEAD
 import CalendarPicker from './components/CalendarPicker';
-=======
-
->>>>>>> fc26f039f410b48409c0e5e3792f1ee24076cdb7
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,28 +13,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-<<<<<<< HEAD
 
-
-
-export default class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { loading: true };
-    console.ignoredYellowBox = ['Remote debugger'];
-  }
-  async componentWillMount() {
-    await Expo.Font.loadAsync({
-      'Roboto': require('native-base/Fonts/Roboto.ttf'),
-      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-      'Ionicons': require('@expo/vector-icons/fonts/Ionicons.ttf'),
-    });
-    this.setState({ loading: false });
-  }
-
-
-=======
 
 
 
@@ -59,7 +34,7 @@ export default class App extends React.Component {
   }
 
 
->>>>>>> fc26f039f410b48409c0e5e3792f1ee24076cdb7
+
   render() {
     if (this.state.loading) {
       return <Expo.AppLoading />;
@@ -68,20 +43,12 @@ export default class App extends React.Component {
     return (
       <Provider store={store}>
       <View style={styles.container}>
-<<<<<<< HEAD
 	<CalendarPicker/>
 	
-=======
 
->>>>>>> fc26f039f410b48409c0e5e3792f1ee24076cdb7
       </View>
       </Provider>
     );
   }
-<<<<<<< HEAD
 }
 }
-=======
-}
-}
->>>>>>> fc26f039f410b48409c0e5e3792f1ee24076cdb7
