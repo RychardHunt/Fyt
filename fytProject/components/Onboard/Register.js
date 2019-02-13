4748de09888a415fb4531180948f70246a5dd383
RychardHunt <wguo24@buffalo.edu>
2019-02-13 15:48:54 -0500

    WIP - Upload to get updated screen

---
+++ fytProject/components/Onboard/Register.js
@@ -0,0 +1,11 @@
+// import * as firebase from "firebase";
+
+const firebaseConfig = require("../../config/firebaseconfig.json");
+
+console.log(firebaseConfig);
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: DATABASE_URL
+//   // databaseURL: "https://fytapp-aef50.firebaseio.com"
+// });
